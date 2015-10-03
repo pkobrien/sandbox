796bae0cbe0337cfd39b43fb9b567dc8a08a9070
Patrick K. O'Brien <patrick.keith.obrien@gmail.com>
2015-10-03 14:15:34 -0500

    benchmarking

--- quil_sketches/project.clj
+++ quil_sketches/project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [criterium "0.4.3"]
                  [quil "2.2.6"]])
--- quil_sketches/src/quil_sketches/ergo_ca.clj
+++ quil_sketches/src/quil_sketches/ergo_ca.clj
@@ -1,5 +1,6 @@
 (ns quil-sketches.ergo-ca
-  (:require [ion.ergo.core :as ergo]
+  (:require [criterium.core :as cr]
+            [ion.ergo.core :as ergo]
             [quil.core :as q]
             [quil.middleware :as m]))
 
@@ -47,21 +48,136 @@
   ([age color state]
    (->Cell age color state)))
 
-(defn make-seed [x y]
-  (vec (map vec (partition y (take (* x y) (repeatedly make-cell))))))
+(defn make-seed [w h]
+  (vec (map vec (partition h (take (* w h) (repeatedly make-cell))))))
 
-(defn create-cell [gen grid x y cell]
-  (let [neighborhood (keep #(get-in grid %) (ergo/neighborhood-4 [x y]))
-        color (/ (reduce + (map :color neighborhood)) (count neighborhood))]
-    (->Cell (inc (:age cell)) color (:state cell))))
+
+(def neighborhood-4-x (juxt inc identity dec identity))
+(def neighborhood-4-y (juxt identity inc identity dec))
+
+(defn neighborhood-4-tx [tx]
+  (juxt (comp tx inc) identity (comp tx dec) identity))
+
+(defn neighborhood-4-ty [ty]
+  (juxt identity (comp ty inc) identity (comp ty dec)))
+
+(defn neighborhood-4
+  "Returns a vector of the 4 [x y] pairs of a von Neumann neighborhood."
+  ([[x y]]
+   (map vector (neighborhood-4-x x) (neighborhood-4-y y)))
+  ([tx ty [x y]]
+   (map vector ((neighborhood-4-tx tx) x) ((neighborhood-4-ty ty) y))))
+
+(def neighborhood-8-x (juxt inc inc identity dec dec dec identity inc))
+(def neighborhood-8-y (juxt identity inc inc inc identity dec dec dec))
+
+(defn neighborhood-8-tx [tx]
+  (let [t-inc (comp tx inc)
+        t-dec (comp tx dec)]
+    (juxt t-inc t-inc identity t-dec t-dec t-dec identity t-inc)))
+
+(defn neighborhood-8-ty [ty]
+  (let [t-inc (comp ty inc)
+        t-dec (comp ty dec)]
+    (juxt identity t-inc t-inc t-inc identity t-dec t-dec t-dec)))
+
+(defn neighborhood-8
+  ([[x y]]
+   (map vector (neighborhood-8-x x) (neighborhood-8-y y)))
+  ([tx ty [x y]]
+   (map vector ((neighborhood-8-tx tx) x) ((neighborhood-8-ty ty) y))))
+
+
+(defn create-cell [gen w h grid tx ty x y cell]
+  (let [neighbors (map #(get-in grid %) (neighborhood-4 tx ty [x y]))
+        avg-color (/ (reduce + (map :color neighbors)) (count neighbors))]
+    (->Cell (inc (:age cell)) avg-color (:state cell))))
+
+(defn create-aged-cell [gen w h grid tx ty x y cell]
+  (->Cell (inc (:age cell)) (:color cell) (:state cell)))
+
+(defn create-colored-4-cell [gen w h grid tx ty x y cell]
+  (let [neighbors (map #(get-in grid %) (neighborhood-4 tx ty [x y]))
+        avg-color (/ (reduce + (map :color neighbors)) (count neighbors))]
+    (->Cell (inc (:age cell)) avg-color (:state cell))))
+
+#_(defn create-colored-8-cell
+  "Returns a cell whose color is a slow move towards the average of its
+   neighbors."
+  [gen w h grid tx ty x y cell]
+  (let [neighbors (map #(get-in grid %) (neighborhood-8 tx ty [x y]))
+        avg-color (/ (reduce + (map :color neighbors)) (count neighbors))]
+    (->Cell (inc (:age cell)) (/ (+ (:color cell) avg-color) 2) (:state cell))))
+
+(defn create-colored-8-cell
+  "Returns a cell whose color the average of its own and a random neighbor."
+  [gen w h grid tx ty x y cell]
+  (let [neighbors (map #(get-in grid %) (neighborhood-8 tx ty [x y]))
+        random-color (rand-nth (map :color neighbors))]
+    (->Cell (inc (:age cell)) (/ (+ (:color cell) random-color) 2) (:state cell))))
 
 (defn make-ca-system
-  [seed]
-  (ca-2d-grid-system
-    seed
-    ->Grid-2D
-    (ergo/gen (fn get-cell-fn [gen grid]
-                (partial create-cell gen grid)))))
+  [w h]
+  (let [tx (fn toroidal-x [x] (mod x w))
+        ty (fn toroidal-y [y] (mod y h))]
+    (ca-2d-grid-system
+      (make-seed w h)
+      ->Grid-2D
+      (ergo/gen (fn get-cell-fn [gen grid]
+                  (partial create-cell gen w h grid tx ty))))))
+
+(defn make-ca-system-aged
+  [w h]
+  (let [tx (fn toroidal-x [x] (mod x w))
+        ty (fn toroidal-y [y] (mod y h))]
+    (ca-2d-grid-system
+      (make-seed w h)
+      ->Grid-2D
+      (ergo/gen (fn get-cell-fn [gen grid]
+                  (partial create-aged-cell gen w h grid tx ty))))))
+
+(defn make-ca-system-colored-4
+  [w h]
+  (let [tx (fn toroidal-x [x] (mod x w))
+        ty (fn toroidal-y [y] (mod y h))]
+    (ca-2d-grid-system
+      (make-seed w h)
+      ->Grid-2D
+      (ergo/gen (fn get-cell-fn [gen grid]
+                  (partial create-colored-4-cell gen w h grid tx ty))))))
+
+(defn make-ca-system-colored-8
+  [w h]
+  (let [tx (fn toroidal-x [x] (mod x w))
+        ty (fn toroidal-y [y] (mod y h))]
+    (ca-2d-grid-system
+      (make-seed w h)
+      ->Grid-2D
+      (ergo/gen (fn get-cell-fn [gen grid]
+                  (partial create-colored-8-cell gen w h grid tx ty))))))
+
+
+; ------------------------------------------------------------------------------
+; Benchmarking
+
+(comment
+
+  (cr/with-progress-reporting ; Unoptimized: 5.87 sec
+    (cr/quick-bench (-> (make-ca-system 72 36) (nth 60)) :verbose))
+
+  (cr/with-progress-reporting ; Make seeds: 214 ms
+    (cr/quick-bench (dorun 60 (repeatedly #(make-seed 72 36))) :verbose))
+
+  (cr/with-progress-reporting ; Only increments age: 344 ms
+    (cr/quick-bench (-> (make-ca-system-aged 72 36) (nth 60)) :verbose))
+
+  (cr/with-progress-reporting ; Color average of 4 neighbors: 5.87 sec
+    (cr/quick-bench (-> (make-ca-system-colored-4 72 36) (nth 60)) :verbose))
+
+  (cr/with-progress-reporting ; Color average of 8 neighbors: 14.33 sec
+    (cr/quick-bench (-> (make-ca-system-colored-8 72 36) (nth 60)) :verbose))
+
+  )
 
 
 ; ------------------------------------------------------------------------------
@@ -82,10 +198,24 @@
       (q/fill (:color cell) 100 100)
       (q/rect (* size x) (+ (* size y) top) size size radius))))
 
+#_(defn draw-cells [state]
+  (q/stroke 0)
+  (let [radius (:cell-radius state)
+        size (:cell-size state)
+        top (:top-margin state)
+        grid (.-grid (:grid state))
+        draw (fn [x y cell]
+               (q/fill (:color cell) 100 100)
+               (q/rect (* size x) (+ (* size y) top) size size radius))
+        inner (fn [x column]
+                (dorun (map-indexed (fn [y cell] (draw x y cell)) column)))]
+    (dorun (map-indexed inner grid))))
+
 (defn draw-text [state]
   (q/fill 0)
   (q/text-size 20)
-  (q/text (str "FPS: " (int (q/current-frame-rate))) 10 30))
+  (q/text (str "Frame: " (q/frame-count)) 10 30)
+  (q/text (str "FPS: " (int (q/current-frame-rate))) 10 60))
 
 (defn draw-state [state]
   (q/background 220)
@@ -108,12 +238,12 @@
     :features [:keep-on-top]
     :middleware [m/fun-mode]))
 
-(defn setup-opts []
-  {:frame-rate 2
+(defn setup-opts [fps]
+  {:frame-rate fps
    :color-mode [:hsb 360 100 100]})
 
-(defn get-state []
-  {:ca-seq (make-ca-system (make-seed 72 36))
+(defn get-state [system]
+  {:ca-seq system
    :cell-radius 2
    :cell-size 10
    :frame 0
@@ -124,6 +254,12 @@
 
 (comment
 
-  (sketch (setup-opts) (get-state))
+  (sketch (setup-opts 20) (get-state (make-ca-system 72 36)))
+
+  (sketch (setup-opts 20) (get-state (make-ca-system-aged 72 36)))
+
+  (sketch (setup-opts 20) (get-state (make-ca-system-colored-4 72 36)))
+
+  (sketch (setup-opts 1) (get-state (make-ca-system-colored-8 72 36)))
 
   )
