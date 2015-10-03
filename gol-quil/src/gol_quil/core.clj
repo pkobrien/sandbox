cbd5cfe28e83c9bb12615f2de0144af346b0eb81
Patrick K. O'Brien <patrick.keith.obrien@gmail.com>
2015-10-03 07:46:39 -0500

    wip

--- gol-quil/src/gol_quil/core.clj
+++ gol-quil/src/gol_quil/core.clj
@@ -2,14 +2,84 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(defn get-seed []
-  #{; Acorn
-    [70 62] [71 60] [71 62] [73 61] [74 62] [75 62] [76 62]
-    })
+
+;; -- Constants ----------------------------------------------------------------
+
+(def alpha-seed :random)  ;; usually :acorn or :random
+
+(def alpha-state
+  {:cell-radius 2
+   :cell-size 8
+   :cells {}
+   :frame 0
+   :generation 0
+   :running true})
+
+(def seeds
+  {:acorn   #{[70 62] [71 60] [71 62] [73 61] [74 62] [75 62] [76 62]}
+   :foo     #{[1 1]}
+   :foo-bar #{[1 1] [2 1] [1 2] [2 3] [3 4] [1 4]}})
+
+
+;; -- Functions ----------------------------------------------------------------
+
+;; (defn neighbours
+;;   "Given a cell's coordinates, returns the coordinates of its neighbours."
+;;   [[x y]]
+;;   (for [dx [-1 0 1] dy (if (zero? dx) [-1 1] [-1 0 1])]
+;;     [(+ dx x) (+ dy y)]))
+
+;; (defn step
+;;   "Given a set of living cells, computes the new set of living cells."
+;;   [cells]
+;;   (set (for [[cell n] (frequencies (mapcat neighbours cells))
+;;              :when (or (= n 3) (and (= n 2) (cells cell)))]
+;;          cell)))
+
+
+
+;; (defn neighbors [[x y]]
+;;   (for [dx [-1 0 1]
+;;         dy (if (zero? dx) [-1 1] [-1 0 1])]
+;;     [(+ dx x) (+ dy y)]))
+
+;; (defn changed-cells [[x y]]
+;;   (for [dx [-1 0 1]
+;;         dy  [-1 0 1]]
+;;     [(+ dx x) (+ dy y)]))
+
+;; (defn step [state]
+;;   (let [{:keys [cells changed]} state
+;;         changed-neighborhood (set (mapcat changed-cells-memo changed))
+;;         changed-and-alive (s/intersection changed-neighborhood cells)
+;;         changed-cells (frequencies (mapcat neighbors-memo changed-and-alive))
+;;         live-counts (reduce (fn [acc loc] (assoc acc loc 0)) {} changed-and-alive)
+;;         changed-cells (merge live-counts changed-cells)
+;;         state (assoc state :changed #{})]
+;;     (reduce (fn [acc [loc n]]
+;;               (cond (and (cells loc) (or (< n 2) (> n 3)))
+;;                     (-> acc
+;;                         (update-in [:cells] disj loc)
+;;                         (update-in [:changed] conj loc))
+;;                     (and (nil? (cells loc)) (= n 3))
+;;                     (-> acc
+;;                         (update-in [:cells] conj loc)
+;;                         (update-in [:changed] conj loc))
+;;                     :else acc)) state changed-cells)))
+
 
 (defn neighbors [[x y]]
   (map vector ((juxt inc      inc identity dec dec      dec identity inc) x)
               ((juxt identity inc inc      inc identity dec dec      dec) y)))
+
+(defn random-seed [n max-x max-y offset-x offset-y]
+  (set (repeatedly n #(vector (+ (rand-int max-x) offset-x)
+                              (+ (rand-int max-y) offset-y)))))
+
+(defn get-seed [seed]
+  (if (= seed :random)
+    (random-seed 1600 100 100 0 0)
+    (seeds seed)))
 
 ;; (defn get-colors [k cells]
 ;;   (map #(:color %1) (keep #(get cells %1) (neighbors k))))
@@ -18,25 +88,54 @@
   ([]
    {:age 0 :color {:r (rand-int 256) :g (rand-int 256) :b (rand-int 256)}})
   ([k cells]
-   ; Set the color of this newborn cell using a blend of the colors of its 3 living neighbors.
-   (let [[color1 color2 color3] (map #(:color %1) (keep #(get cells %1) (neighbors k)))]
-     {:age 0 :color {:r (:r color1) :g (:g color2) :b (:b color3)}})))
+   ; Set newborn cell color using a blend of colors of its 3 living neighbors.
+   (let [[c1 c2 c3] (map #(:color %1) (keep #(get cells %1) (neighbors k)))]
+     {:age 0 :color {:r (:r c1) :g (:g c2) :b (:b c3)}})))
 
 (defn step [cells]
-  (into (hash-map) (for [[k n] (frequencies (mapcat neighbors (keys cells)))
-                         :when (or (= n 3) (and (= n 2) (contains? cells k)))]
-                     [k (update-in (or (get cells k) (create-cell k cells)) [:age] inc)])))
+  (into {} (for [[k n] (->> (keys cells) (mapcat neighbors) (frequencies))
+                 :let [cell (or (get cells k) (create-cell k cells))]
+                 :when (or (= n 3) (and (= n 2) (contains? cells k)))]
+             [k (update-in cell [:age] inc)])))
+
+
+;; -- Draw ---------------------------------------------------------------------
+
+(defn draw-cells [state]
+  (q/stroke 0)
+  (let [r (:cell-radius state)
+        w (:cell-size   state)]
+    (doseq [[[x y] cell] (:cells state)
+            :let [color (:color cell)]]
+      ;(q/fill (min (:age cell) 255))
+      (q/fill (:r color) (:g color) (:b color))
+      (q/rect (* w x) (* w y) w w r))))
+
+(defn draw-text [state]
+  (q/fill 0)
+  (q/text-size 20)
+  (q/text (str "Generation: "  (:generation state)) 20 40)
+  (q/text (str "Population: "  (count (:cells state))) 20 60)
+  (q/text (str "Oldest Age: "  (apply max (map #(:age %1) ((fnil vec [{:age "N/A"}]) (vals (:cells state)))))) 20 80)
+;  (q/text (str "Oldest Age: "  (apply (fnil max 0) (vec (map #(:age %1) (vals (:cells state)))))) 20 80)
+  (q/text (str "Frame:      "  (:frame state)) 20 120)
+  (q/text (str "Frame Rate: "  (q/current-frame-rate)) 20 140)
+  (q/text (str "Running:    "  (:running state)) 20 160))
+
+
+;; -- Setup --------------------------------------------------------------------
 
 (defn setup-cells [seed]
-  (into (hash-map) (map #(conj [%1] (create-cell)) seed)))
+  (into {} (for [k seed] [k (create-cell)])))
+
+
+;; -- Quil Handlers ------------------------------------------------------------
 
 (defn setup []
   (q/frame-rate 60)
-  {:cell-size 8
-   :cells (setup-cells (get-seed))
-   :frame 0
-   :generation 0
-   :running true})
+  (->> (get-seed alpha-seed)
+       (setup-cells)
+       (assoc alpha-state :cells)))
 
 (defn update [state]
   (let [state (update-in state [:frame] inc)]
@@ -48,20 +147,8 @@
 
 (defn draw [state]
   (q/background 120)
-  (q/stroke 0)
-  (q/fill 0)
-  (q/text-size 20)
-  (q/text (str "Generation: "  (:generation state)) 20 40)
-  (q/text (str "Population: "  (count (:cells state))) 20 60)
-  (q/text (str "Oldest Age: "  (apply max (map #(:age %1) (vals (:cells state))))) 20 80)
-  (q/text (str "Frame:      "  (:frame state)) 20 120)
-  (q/text (str "Frame Rate: "  (q/current-frame-rate)) 20 140)
-  (q/text (str "Running:    "  (:running state)) 20 160)
-  (doseq [[[x y] cell] (:cells state)
-          :let [w (:cell-size state) color (:color cell)]]
-    ;(q/fill (min (:age cell) 255))
-    (q/fill (:r color) (:g color) (:b color))
-    (q/rect (* w x) (* w y) w w 2)))
+  (draw-cells state)
+  (draw-text state))
 
 (defn mouse-clicked [state event]
   (update-in state [:running] not))
